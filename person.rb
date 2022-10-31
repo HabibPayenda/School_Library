de414a3bc9a8490aae9b40204b09d2ae40a32e19
Habib Payenda <habiburahmanpayenda@gmail.com>
2022-10-31 18:07:57 +0430

    Changes the setters and getters syntax

--- person.rb
+++ person.rb
@@ -1,29 +1,12 @@
 class Person
+  attr_accessor :name, :age
+  attr_reader :id
+
   def initialize(age, name = 'Unknown', parent_permission: true)
     @id = Random.rand(1..100)
     @name = name
     @age = age
     @parent_permission = parent_permission
-  end
-
-  def age_reader
-    @age
-  end
-
-  def name_reader
-    @name
-  end
-
-  def id_reader
-    @id
-  end
-
-  def age_writer(age)
-    @age = age
-  end
-
-  def name_writer(name)
-    @name = name
   end
 
   def can_use_services?
