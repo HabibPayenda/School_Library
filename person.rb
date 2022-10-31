67ac3c0350b5266539c7b6da82b2d543752cbdbb
Habib Payenda <habiburahmanpayenda@gmail.com>
2022-10-31 17:29:54 +0430

    Adds the required classes

--- person.rb
+++ person.rb
@@ -1,1 +1,42 @@
+class Person
+  def initialize(age, name = 'Unknown', parent_permission: true)
+    @id = Random.rand(1..100)
+    @name = name
+    @age = age
+    @parent_permission = parent_permission
+  end
 
+  def age_reader
+    @age
+  end
+
+  def name_reader
+    @name
+  end
+
+  def id_reader
+    @id
+  end
+
+  def age_writer(age)
+    @age = age
+  end
+
+  def name_writer(name)
+    @name = name
+  end
+
+  def can_use_services?
+    if of_age? || @parent_permission
+      true
+    else
+      false
+    end
+  end
+
+  private
+
+  def of_age?
+    true unless @age < 18
+  end
+end
--- student.rb
+++ student.rb
@@ -1,1 +1,12 @@
+require_relative 'person.rb'
 
+class Student < Person
+  def initialize(age, class_room, name = 'Unknown', parent_permission: true)
+    super(age, name, parent_permission)
+    @class_room = class_room
+  end
+
+  def play_hooky
+    '¯(ツ)/¯'
+  end
+end
