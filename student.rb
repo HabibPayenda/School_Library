dfa6e30dab9b6590c8ef9ef22131c6ba83a6dd8b
Habib Payenda <habiburahmanpayenda@gmail.com>
2022-11-02 16:12:36 +0430

    Adds the associations and new classes

--- student.rb
+++ student.rb
@@ -3,10 +3,15 @@ require_relative 'person'
 class Student < Person
   def initialize(age, class_room, name = 'Unknown', parent_permission: true)
     super(age, name, parent_permission)
-    @class_room = class_room
+    @classroom = class_room
   end
 
   def play_hooky
     '¯(ツ)/¯'
+  end
+
+  def classroom=(classroom)
+    @classroom = classroom
+    classroom.students.push(self) unless classroom.students.include?(self)
   end
 end
